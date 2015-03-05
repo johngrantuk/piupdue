30b3b3cc3c9f04cc457dbdde0aaa5b601a80fa7f
John <johngrantuk@googlemail.com>
2015-03-05 08:41:04 +0000

    Added some scaffolding.

---
+++ setup.py
@@ -0,0 +1,53 @@
+from __future__ import print_function
+from setuptools import setup, find_packages
+import io
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.txt', 'CHANGES.txt')
+
+setup(
+    name='piupdue',
+    version=sandman.__version__,
+    url='http://github.com/jeffknupp/sandman/',
+    license='Apache Software License',
+    author='Jeff Knupp',
+    tests_require=['pytest'],
+    install_requires=['Flask>=0.10.1',
+                    'Flask-SQLAlchemy>=1.0',
+                    'SQLAlchemy==0.8.2',
+                    ],
+    cmdclass={'test': PyTest},
+    author_email='jeff@jeffknupp.com',
+    description='Automated REST APIs for existing database-driven systems',
+    long_description=long_description,
+    packages=['sandman'],
+    include_package_data=True,
+    platforms='any',
+    test_suite='sandman.test.test_sandman',
+    classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        ],
+    extras_require={
+        'testing': ['pytest'],
+    }
+)
